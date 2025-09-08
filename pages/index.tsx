26b6943775bb7122cc123425edc026542599be91
falconstore <53540785+falconstore@users.noreply.github.com>
2025-09-08 16:35:12 -0300

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Inter } from 'next/font/google'
+import JNFinancasSystem from '@/components/JNFinancasSystem'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className={`${inter.className}`}>
+      <JNFinancasSystem />
+    </main>
+  )
+}
